7e74176894131ed66ca15c7a139364ebea1179cb
Joaquin <jachaval@users.noreply.github.com>
2021-07-07 12:25:19 -0300

    Base de datos actualizada
    Se agregan las tablas empleados clientes y empleados
    Se elimina persona
    Se hacen los id de Tipos, prioridades, perfiles como identity

--- CALLCENTER_DB.sql
+++ CALLCENTER_DB.sql
@@ -6,36 +6,60 @@ go
 use CALLCENTER_DB
 GO
 CREATE TABLE PERFILES(
-	ID int not null primary key,
+	ID int not null primary key identity(1,1),
 	TipoPerfil varchar(20)
 )
 
 GO
 CREATE TABLE ESTADOS(
-	ID int not null primary key,
+	ID int not null primary key identity(1,1),
 	EstadoIncidente varchar(20)
 )
 GO
 CREATE TABLE TIPOS(
-	ID int not null primary key,
+	ID int not null primary key identity(1,1),
 	TipoIncidente varchar(100)
 )
 GO
 CREATE TABLE PRIORIDADES(
-	ID int not null primary key,
+	ID int not null primary key identity(1,1),
 	PrioridadIncidente varchar(100)
 )
 GO 
-CREATE TABLE PERSONAS(
+CREATE TABLE CLIENTES(
+	ID int not null primary key identity(1,1),
 	Nombre varchar(50) not null,
 	Apellido varchar(50) not null,
-	Email varchar(150),
+	Email varchar(150) unique not null,
 	Telefono varchar(50),
-	DNI varchar(20)
+	DNI varchar(20) unique
+)
+GO 
+CREATE TABLE EMPLEADOS(
+	ID int not null primary key identity(1,1),
+	Nombre varchar(50) not null,
+	Apellido varchar(50) not null,
+	Email varchar(150) unique not null,
+	Telefono varchar(50),
+	DNI varchar(20) unique,
+	IDPerfil int not null foreign key references PERFILES(ID)
 )
 GO
-insert into PERFILES values ('1','Administrador'), ('2','Telefonista'), ('3','Supervisor')
+CREATE TABLE INCIDENTES(
+	Numero int not null primary key identity (1,1),
+	Asunto varchar(500),
+	Comentario varchar(500),
+	Fecha datetime not null,
+	IDCliente int not null,
+	IDEmpleado int not null,
+	IDPrioridad int not null,
+	IDTipo int not null,
+	IDEstado int not null
+)
 
 GO
-insert into ESTADOS values ('1','Abierto'), ('2','En Análisis'), ('3','Cerrado'), ('4','Reabierto'), ('5','Asignado'), ('6','Resuelto')
+insert into PERFILES values ('Administrador'), ('Telefonista'), ('Supervisor')
+
+GO
+insert into ESTADOS values ('Abierto'), ('En Análisis'), ('Cerrado'), ('Reabierto'), ('Asignado'), ('Resuelto')
 
